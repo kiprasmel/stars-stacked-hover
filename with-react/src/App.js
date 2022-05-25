2afbec64eecb9744a7d2ff972902941427af8cb9
Kipras Melnikovas <kipras@kipras.org>
2022-05-25 05:26:20 +0300

    add link to fork on github
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- with-react/src/App.js
+++ with-react/src/App.js
@@ -1,29 +1,39 @@
+import ForkMeOnGithub from "fork-me-on-github";
+
 import { StarsIndividual, StarsStacked } from "./stars";
 
 function App() {
 	return (
-		<div
-			style={{
-				textAlign: "center",
-				display: "flex",
-				flexDirection: "column",
-				justifyContent: "center",
-				margin: "auto",
-				minHeight: "100vh",
-			}}
-		>
-			<div>
-				<p>individual</p>
-				<StarsIndividual count={5} />
+		<div>
+			<div
+				style={{
+					textAlign: "center",
+					display: "flex",
+					flexDirection: "column",
+					justifyContent: "center",
+					margin: "auto",
+					minHeight: "100vh",
+				}}
+			>
+				<div>
+					<p>individual</p>
+					<StarsIndividual count={5} />
+				</div>
+				<div style={{ marginTop: "8px" }}>
+					<p>stacked, stopPropagation = false</p>
+					<StarsStacked count={5} stopPropagation={false} />
+				</div>
+				<div style={{ marginTop: "8px" }}>
+					<p>stacked, stopPropagation = true</p>
+					<StarsStacked count={5} stopPropagation={true} />
+				</div>
 			</div>
-			<div style={{ marginTop: "8px" }}>
-				<p>stacked, stopPropagation = false</p>
-				<StarsStacked count={5} stopPropagation={false} />
-			</div>
-			<div style={{ marginTop: "8px" }}>
-				<p>stacked, stopPropagation = true</p>
-				<StarsStacked count={5} stopPropagation={true} />
-			</div>
+
+			<ForkMeOnGithub
+				repo="https://github.com/kiprasmel/stars-stacked-hover"
+				colorBackground="black"
+				colorOctocat="white"
+			/>
 		</div>
 	);
 }
